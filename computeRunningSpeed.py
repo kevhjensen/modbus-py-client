0739bbcb11af084a9c1a8079e072bf68ab89a4ff
Kevin Jensen <kevinj@PHUS.CORP>
2025-03-06 12:43:29 -0800

    if no pwd input, dont send pwd; improve cylical

--- computeRunningSpeed.py
+++ computeRunningSpeed.py
@@ -1,35 +1,50 @@
-# from ChgModbusLib import pyZerovaChgrModbus
-# import time
-# #import matplotlib.pyplot as plt
+from ChgModbusLib import pyZerovaChgrModbus
+import time
 
 
-# modbus = pyZerovaChgrModbus()
-# modbus.connect('192.168.100.1', "")
+modbus = pyZerovaChgrModbus()
 
-# total_time = 0
-# num_of_loop = 100
-# #time_elapsed_list = []
-# setPoints = [60, 50, 60, 55, 50, 55, 60, 59, 58, 57, 56, 55, 56, 57, 58, 59, 60]
+print(modbus.connect('192.168.10.111'))
 
-# for sP in setPoints:
-#     start = time.time()
-#     modbus.writeConfig([0, 0, 0, sP, 0])
-#     #modbus.readConfig()
-#     end = time.time()
-#     time_elapsed = end - start
-#     #time_elapsed_list.append(time_elapsed) 
-#     valid, data = modbus.get_connector_info(5)
-#     #print(data)
-#     total_time += time_elapsed
-#     print(time_elapsed)
+total_time = 0
+num_of_loop = 100
+setpoints = [i for i in range(5)] # + [i for i in range(30, 50, 5)] + [0, 50, 5, 10]
+print(setpoints)
+
+time_elapsed_list = []
+commands = [    #writeconf, readconf, getconn(1-> num), readvolt
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [1, 1, 2, 0],
+    [1, 1, 2, 1],
+    [1, 1, 5, 0],
+    [1, 1, 5, 1],
+
+    ] 
+setpoints = [0, 1]
+for command in commands:
+    for i in range(len(setpoints)):
+        start = time.time()
+        if command[0]:
+            print(modbus.writeConfig([0, 0, 0, setpoints[i], 0, 0, 0, 0, 0]))
+        if command[1]:
+            print(modbus.readConfig())
+        for id in range(1, command[2]):
+            res = modbus.get_connector_info(id)
+            print(f"GetConn({id}). Res: {res[0]}")
+            if not res[0]:
+                print(res[1])
+        if command[3]:
+            print(modbus.read_input_voltage())
+        end = time.time()
+        time_elapsed = end - start
+        time_elapsed_list.append(time_elapsed)
+        print(time_elapsed)
+        print("---") 
+        total_time += time_elapsed
+        # time.sleep(3)
+    print("----------------------")
 
 
-# print("average running time:", total_time/num_of_loop)
-# # Plot the time_elapsed as a line chart
-# # plt.figure(figsize=(10, 6))
-# # plt.plot(range(num_of_loop), time_elapsed_list, marker='o', linestyle='-')
-# # plt.title('Execution Time for Each Loop Iteration')
-# # plt.xlabel('Iteration')
-# # plt.ylabel('Time Elapsed (seconds)')
-# # plt.grid()
-# # plt.show()
+print("average running time:", total_time/num_of_loop)
+print(time_elapsed_list)
